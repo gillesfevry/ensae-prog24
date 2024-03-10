44a745b0a944356b4945f712fcb009178213764d
gillesf <gilles.fevry@ensae.fr>
2024-03-10 12:18:57 +0000

    changement comparaison

--- swap_puzzle/comparaison.py
+++ swap_puzzle/comparaison.py
@@ -1,6 +1,3 @@
-import sys 
-sys.path.append("swap_puzzle/")
-
 from solver import Solver 
 from grid import Grid  
 
@@ -12,15 +9,15 @@ A=Grid(3,3)
 x=[]
 y=[]
 z=[]
+t=[]
 
-for i in range(10):
-    print(i)
+for i in range(100):
+    print(i,"%")
     A.Shuffle()
-    x = x + [len(S.A_star(A, S.Heuristique_Manhattan))]
+    t = t + [len(S.A_star(A, S.Heuristique_Manhattan))]
     y = y + [len(S.naif(A))+1]
     z = z + [len(S.compromis(A, S.Heuristique_Manhattan))]
-    if len(S.A_star(A, S.Heuristique_Manhattan)) > len(S.naif(A))+1:
-        print(A)
+    x = x + [S.Heuristique_Manhattan(A)]
 
 pente, intercept = np.polyfit(x, y, 1)
 plt.plot(x, pente*np.array(x) + intercept, color='blue')
@@ -31,14 +28,21 @@ plt.text(max(x), int(pente*100)/100 * max(x) + int(intercept*100)/100, f'y={int(
 pente, intercept = np.polyfit(x, z, 1)
 plt.plot(x, pente*np.array(x) + intercept, color='red')
 plt.scatter(x, z, color='red')
-plt.text(min(x), min(z), 'A* rapide', verticalalignment='bottom', horizontalalignment='right', color='red')
+plt.text(min(x), min(z), 'compromis', verticalalignment='bottom', horizontalalignment='right', color='red')
 plt.text(max(x), int(pente*100)/100 * max(x) + int(intercept*100)/100 , f'y={int(pente*100)/100} x + {int(intercept*100)/100}', verticalalignment='bottom', horizontalalignment='right', color='red')
+
+pente, intercept = np.polyfit(x, t, 1)
+plt.plot(x, pente*np.array(x) + intercept, color='green')
+plt.scatter(x, t, color='green')
+plt.text(min(x), min(t)-1, 'A_star', verticalalignment='bottom', horizontalalignment='right', color='green')
+plt.text(max(x), int(pente*100)/100 * max(x) + int(intercept*100)/100 , f'y={int(pente*100)/100} x + {int(intercept*100)/100}', verticalalignment='bottom', horizontalalignment='right', color='green')
 
 plt.plot(x, x, color='black')
 plt.text(min(x)+1, min(x), 'A*', verticalalignment='bottom', horizontalalignment='right', color='black')
 plt.text(max(x), max(x)-1, f'y=x', verticalalignment='bottom', horizontalalignment='right', color='black')
 
 plt.title("Comparaison de différents algorithmes pour 100 grilles 3x3:")
-plt.xlabel('Cout de A*')
-plt.ylabel('Cout des autres algorithmes')
+plt.xlabel('Heuristique de Manhattan')
+plt.ylabel('Cout des algorithmes')
+
 plt.show()
